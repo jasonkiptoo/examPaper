ce4b044c0e5b50d75ec5f765be45c528eefe16cb
Jason Kiptoo <Jason.Kiptoo@ncbagroup.com>
2023-10-16 12:27:49 +0300

    initial setup for the project exams

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,58 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header
+      elevated
+      class="text-white"
+      height-hint="98"
+      style="background-color: #809ddf; padding-top: 20px"
+    >
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title> Exams Corner </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="center">
+        <q-route-tab no-caps to="/page1" label="Notes " />
+        <q-route-tab no-caps to="/page2" label="Exam" />
+        <q-route-tab no-caps to="/page3" label="Schemes of Work" />
+        <q-route-tab no-caps to="/page2" label="Q&A" />
+        <q-route-tab no-caps to="/page2" label="Video Lessons" />
+        <q-route-tab no-caps to="/page2" label="Question Papers" />
+        <q-route-tab no-caps to="/page2" label="Colleges" />
+        <q-route-tab no-caps to="/page2" label="Universities" />
+        <q-route-tab no-caps to="/page2" label="Courses" />
+        <q-route-tab no-caps to="/page2" label="Resources" />
+      </q-tabs>
+      <q-tabs align="right">
+        <q-route-tab no-caps to="/page1" label="Login " />
+        <q-route-tab no-caps to="/page1" label="Register " />
+      </q-tabs>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+};
+</script>
---
+++ src/pages/examPage.vue
@@ -0,0 +1,258 @@
+<template>
+  <q-page class="">
+    <div class="col-12 q-pl-md" style="width: 70%">
+      .
+      <div class="col-12">
+        <q-input
+          label="Search for exams here"
+          style="width: 70%"
+          outlined
+          rounded
+        />
+      </div>
+      <div class="col-12">
+        <div class="=" style="width: 60%; color: navy">
+          <h4>
+            Buy model exam papers with answers for primary and high schools in
+            Kenya Find exam papers for schools in Kenya online.
+          </h4>
+          <h6 class="" style="color: black">
+            Get various categories of exams such as opener, mid term, end term
+            as well as mock examinations. The papers are available in both Word
+            and Pdf formats. Marking schemes are also available with every
+            download.
+          </h6>
+        </div>
+        <div class="col-12" style="color: navy">
+          <h4>Exams Categories</h4>
+        </div>
+        <div class="row" id="exam-container">
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+          <div
+            class="col-5 row text-blue"
+            style="cursor: pointer; text-decoration: underline"
+          >
+            Form 1 End of Term 1
+          </div>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ExamPage",
+});
+</script>
+<style scoped>
+#exam-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px; /* Adjust this value to control the gap between items */
+}
+
+.col-5 {
+  width: calc(10% - 10px); /* 10% width for each item with gap */
+  margin-right: 20px; /* Adjust this value to control the gap between items */
+}
+</style>
